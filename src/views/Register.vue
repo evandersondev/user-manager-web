9a1358b10d0a8a782947fdc8e0f347524502b4e4
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2023-09-01 15:40:19 -0300

    create user from web app

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,58 +2,65 @@
 import { reactive } from 'vue'
 import { Image } from 'lucide-vue-next'
 import { useRouter } from 'vue-router'
+import axios from 'axios';
 
 const router = useRouter()
 
-const newUser = reactive({
+const user = reactive({
+  name: '',
+  email: '',
+  password: '',
   photoUrl: '',
   role: 'employee'
 })
 
-function hanldeRegisterSubmit() {
-  console.log(newUser)
-  router.push({ path: '/home' })
+async function hanldeRegisterSubmit() {
+  const response = await axios.post('http://localhost:3000/users', { ...user })
+
+  if (response.status === 201) {
+    router.push({ path: '/' })
+  }
 }
 </script>
 
 <template>
-  <div class="h-screen w-full flex flex-col justify-center items-center">
+  <div class="flex flex-col items-center justify-center w-full h-screen">
     <div class="flex flex-col items-center gap-2 mb-8">
-      <h1 class="text-5xl font-bold tracking-tight text-zinc-800 mb-4">Sign up</h1>
-      <div class="h-20 w-20 rounded-full shadow-sm overflow-hidden bg-zinc-100 flex items-center justify-center">
-        <img v-if="newUser.photoUrl" class="h-full w-full object-cover" :src="newUser.photoUrl" />
-        <Image v-else class="text-zinc-400 text-xl" />
+      <h1 class="mb-4 text-5xl font-bold tracking-tight text-zinc-800">Sign up</h1>
+      <div class="flex items-center justify-center w-20 h-20 overflow-hidden rounded-full shadow-sm bg-zinc-100">
+        <img v-if="user.photoUrl" class="object-cover w-full h-full" :src="user.photoUrl" />
+        <Image v-else class="text-xl text-zinc-400" />
       </div>
 
     </div>
-    <form @submit.prevent="hanldeRegisterSubmit" class="flex flex-col gap-4 w-full max-w-sm">
+    <form @submit.prevent="hanldeRegisterSubmit()" class="flex flex-col w-full max-w-sm gap-4">
 
-      <input v-model="newUser.name" placeholder="Name"
-        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <input v-model="user.name" placeholder="Name"
+        class="w-full h-12 px-6 rounded text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
 
-      <input v-model="newUser.email" type="email" placeholder="E-mail"
-        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <input v-model="user.email" type="email" placeholder="E-mail"
+        class="w-full h-12 px-6 rounded text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
 
-      <input v-model="newUser.password" type="password" placeholder="Password"
-        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <input v-model="user.password" type="password" placeholder="Password"
+        class="w-full h-12 px-6 rounded text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
 
-      <input v-model="newUser.photoUrl" placeholder="Photo URl"
-        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <input v-model="user.photoUrl" placeholder="Photo URl"
+        class="w-full h-12 px-6 rounded text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
 
-      <select v-model="newUser.role" placeholder="Role"
-        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500">
-        <option selected="employee" value="employee">EMPLOYEE</option>
+      <select v-model="user.role" placeholder="Role"
+        class="w-full h-12 px-6 rounded text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500">
+        <option value="employee">EMPLOYEE</option>
         <option value="admin">ADMIN</option>
         <option value="owner">OWNER</option>
       </select>
 
       <button
-        class="h-12 w-full rounded bg-emerald-500 text-zinc-50 font-semibold outline-0 focus:ring-2 focus:ring-emerald-500"
+        class="w-full h-12 font-semibold rounded bg-emerald-500 text-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500"
         type="submit">REGISTER</button>
     </form>
     <div class="flex gap-1 text-sm">
       <p>You have an account?</p>
-      <router-link class="text-emerald-500 font-medium" to="/">Sign in here.</router-link>
+      <router-link class="font-medium text-emerald-500" to="/">Sign in here.</router-link>
     </div>
   </div>
 </template>
