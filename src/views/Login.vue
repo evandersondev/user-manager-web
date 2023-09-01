7799667055d0be8930e929e27bb7ee348192f9a4
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2023-09-01 14:18:02 -0300

    update login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,15 +9,19 @@ function hanldeLoginSubmit() {
 </script>
 
 <template>
-  <div class="h-full w-full flex flex-col space-y-8 justify-center items-center">
-    <div class="flex flex-col items-center">
-      <h1 class="text-4xl font-bold tracking-tight text-zinc-700 sm:text-4xl">lOGIN</h1>
-      <h3 class="text-zinc-400">Hello, Welcome back!</h3>
+  <div class="h-screen w-full flex flex-col space-y-8 justify-center items-center">
+    <div class="flex flex-col items-center gap-2">
+      <h1 class="text-5xl font-bold tracking-tight text-zinc-800">Sign in</h1>
+      <h3 class="text-zinc-500 text-xl">Hello, Welcome back!</h3>
     </div>
-    <form @submit.prevent="hanldeLoginSubmit" class="flex flex-col gap-4 w-full max-w-md">
-      <input placeholder="E-mail" class="h-12 w-full rounded px-6 text-zinc-700 placeholder:text-zinc-300" />
-      <input placeholder="Password" class="h-12 w-full rounded px-6 text-zinc-700 placeholder:text-zinc-300" />
-      <button class="h-12 w-full rounded bg-emerald-500 text-zinc-50 font-semibold" type="submit">ENTER</button>
+    <form @submit.prevent="hanldeLoginSubmit" class="flex flex-col gap-4 w-full max-w-sm">
+      <input placeholder="E-mail"
+        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <input placeholder="Password"
+        class="h-12 w-full rounded px-6 text-zinc-800 placeholder:text-zinc-400 bg-zinc-50 outline-0 focus:ring-2 focus:ring-emerald-500" />
+      <button
+        class="h-12 w-full rounded bg-emerald-500 text-zinc-50 font-semibold outline-0 focus:ring-2 focus:ring-emerald-500"
+        type="submit">ENTER</button>
     </form>
   </div>
 </template>
